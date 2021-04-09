2e232f9bb9aeab92c182a2dce0792438f2f83473
previtus <previtus@gmail.com>
2021-04-09 11:40:21 +0100

    plotting functions for 1 reference and 2 big model experiments

---
+++ ___PLOTTING/plot_1_reference.py
@@ -0,0 +1,73 @@
+from pylab import plt
+from plot_utils import *
+
+experiment_name = "Model replication"
+plot_name = "1_reference_"
+folder = "1_reference_model/logs/"
+piwaes = [folder+"log_PIWAE_M8_k8_repeat10.h5"]
+miwaes = [folder+"log_MIVAE_M8_k8_repeat10.h5"]
+iwaes = [folder+"log_MIVAE_M1_k64_repeat10.h5"]
+vaes = [folder+"log_MIVAE_M1_k1_repeat10.h5"]
+ciwaes = [folder+"log_CIVAE_beta0.5_repeat10.h5"]
+
+### bellow is unchanged ###
+
+def plot_all(select_func, rolling_mean, plot_name, ylabel):
+    all_available = min_epoch([piwaes, miwaes, iwaes, vaes, ciwaes], select_func)
+    print("from all logs we have data at least in epoch", all_available)
+
+    # MIVAE M8, k8
+    p = plot_h5_file_k64_average(piwaes, "PIWAE M=8, k=8", color='#c44e52', at=all_available, select_func=select_func,
+                                 rolling_mean=rolling_mean)
+    m = plot_h5_file_k64_average(miwaes, "MIWAE M=8, k=8", color='#8172b2', at=all_available, select_func=select_func,
+                                 rolling_mean=rolling_mean)
+    i = plot_h5_file_k64_average(iwaes, "IWAE k=64", color='#4c72b0', at=all_available, select_func=select_func,
+                                 rolling_mean=rolling_mean)
+    v = plot_h5_file_k64_average(vaes, "VAE", color='#ccb974', at=all_available, select_func=select_func,
+                                 rolling_mean=rolling_mean)
+    c = plot_h5_file_k64_average(ciwaes, "CIWAE beta=0.5", color='#55a868', at=all_available, select_func=select_func,
+                                 rolling_mean=rolling_mean)
+
+    # plt.ylim(-92,-83)
+    #plt.ylim(-94.5, -83)
+    plt.ylim(-94, -82.5)
+
+    # plt.xlim(0,2300)
+    plt.xlim(0, all_available)
+
+    plt.xlabel("Epoch")
+    plt.ylabel(ylabel)
+
+    plt.legend(loc='lower right')
+    plt.tight_layout()
+
+    plt.savefig(plot_name+".png", dpi=120)
+    plt.savefig(plot_name+".pdf", dpi=120)
+
+    plt.show()
+    plt.close()
+
+    print("============(average from last 20 epochs)======================")
+    print("PIWAE M=8, k=8       ", p)
+    print("MIWAE M=8, k=8       ", m)
+    print("IWAE k=64       ", i)
+    print("VAE       ", v)
+    print("CIWAE beta=0.5       ", c)
+
+
+### Figure 5 reconstruction
+
+select_iwae_64 = lambda x: x[1]
+plt.figure(figsize=(8,4))
+plt.title("Figure 5.a replication, IWAE64; "+experiment_name)
+select_func = select_iwae_64
+rolling_mean = 10
+plot_all(select_func,rolling_mean,plot_name+"_iwae64", ylabel="IWAE-64")
+
+
+select_iwae_5000 = lambda x: x[2]
+plt.figure(figsize=(8,4))
+plt.title("Figure 5.b replication, log p̂(x) (= IWAE5000); "+experiment_name)
+select_func = select_iwae_5000
+rolling_mean = None
+plot_all(select_func,rolling_mean,plot_name+"_iwae5000", ylabel="log p̂(x)")
